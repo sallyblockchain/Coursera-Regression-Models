6d9eb475e0376d8d24672ebcbd53b36c8ad3c473
Xiaodan Zhang <zhang349@illinois.edu>
2015-01-29 00:09:25 -0600

    Updated the last part

--- week4/week4.R
+++ week4/week4.R
@@ -44,4 +44,16 @@ summary(fit2)$coef
 # The coefficient estimate is unchanged
 
 # Problem 6.
-
+x <- -5 : 5
+y <- c(5.12, 3.93, 2.67, 1.87, 0.52, 0.08, 0.93, 2.05, 2.54,
+       3.87, 4.97)
+knotPoint <- c(0)
+spline <- sapply(knotPoint, function(knot) (x > knot) * (x - knot))
+xMatrix <- cbind(1, x, spline)
+fit <- lm(y ~ xMatrix - 1)
+yhat <- predict(fit)
+yhat
+slope <- fit$coef[2] + fit$coef[3]
+slope # 1.013
+plot(x, y)
+lines(x, yhat, col=2)
